0ce5ccf8f49187e476c3515787aa90a0fc25e207
John Beynon <john@render.com>
2026-02-15 18:18:33 +0000

    Attachments

--- src/index.js
+++ src/index.js
@@ -9,8 +9,30 @@ export default {
     console.log('Subject', email.subject);
     console.log('HTML', email.html);
 
+	attachments = []
+
     email.attachments.forEach((attachment) => {
-      console.log('Attachment', attachment.filename, attachment.content);
+      const attachment = {
+		filename: attachment.filename,
+		attachment: attachment.content
+	  }
+	  attachments.push(attachment)
     });
+
+	const emailContent = {
+        subject: email.subject,
+        from: email.html,
+        body: rawEmail,
+		attachments: attachments
+      };
+
+      // Post the email to the webhook
+      await fetch(webhookURL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(emailContent),
+      });
+
+
   },
 };
